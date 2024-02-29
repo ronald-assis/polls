ead43c11c97b666331b91e3f8cbb3fdc8a71f986
Ronald Assis <ronaldassis71@gmail.com>
2024-02-29 12:51:42 -0300

    feat: adeding eslint and zod in the project

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,7 +1,19 @@
 import fastify from "fastify";
+import { z } from "zod"
 
 const app = fastify()
 const PORT = 3333
+
+app.post('/poll', (request) => {
+    const createPollBody = z.object({
+        title: z.string()
+    })
+
+    const { title } = createPollBody.parse(request.body)
+
+    console.log(title)
+    return "it'll work"
+})
 
 app.listen({port: PORT}).then(() =>  {
     console.log(`HTTP server running in ${PORT}`)
