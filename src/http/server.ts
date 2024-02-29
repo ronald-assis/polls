43eb3f614ad62a65e9afd01513773495a2586ffa
Ronald Assis <ronaldassis71@gmail.com>
2024-02-29 12:53:43 -0300

    feat: adjusting spaces

--- src/http/server.ts
+++ src/http/server.ts
@@ -5,16 +5,16 @@ const app = fastify()
 const PORT = 3333
 
 app.post('/poll', (request) => {
-    const createPollBody = z.object({
-        title: z.string()
-    })
+  const createPollBody = z.object({
+    title: z.string()
+  })
 
-    const { title } = createPollBody.parse(request.body)
+  const { title } = createPollBody.parse(request.body)
 
-    console.log(title)
-    return "it'll work"
+  console.log(title)
+  return "it'll work"
 })
 
 app.listen({port: PORT}).then(() =>  {
-    console.log(`HTTP server running in ${PORT}`)
+  console.log(`HTTP server running in ${PORT}`)
 })
